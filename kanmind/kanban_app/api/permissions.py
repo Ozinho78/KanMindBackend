ffab371ae50703265945f7a5dd561700213d44d2
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-08-12 00:26:09 +0200

    comments delete tested

--- kanmind/kanban_app/api/permissions.py
+++ kanmind/kanban_app/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsBoardOwnerOrMember(BasePermission):
@@ -9,6 +10,7 @@ class IsBoardOwnerOrMember(BasePermission):
     def has_object_permission(self, request, view, obj):
         """Attributes needs to be checked to avoid unwanted error messages"""
         
+        # Determine board object (Board | Task | Comment)
         # Attributes for board
         if hasattr(obj, "owner") and hasattr(obj, "members"):
             board = obj
@@ -21,9 +23,14 @@ class IsBoardOwnerOrMember(BasePermission):
         elif hasattr(obj, "task") and hasattr(obj.task, "board"):
             board = obj.task.board
         else:
-            raise AuthenticationFailed(self.message)
+            # raise AuthenticationFailed(self.message)
+            raise PermissionDenied(self.message)
 
+        # Allow access if the user is the owner or member of the board
         if board.owner == request.user or request.user in board.members.all():
             return True
 
-        raise AuthenticationFailed(self.message)
+        # Logged in but no access
+        # raise AuthenticationFailed(self.message)
+        # Why fail if you can deny someone
+        raise PermissionDenied(self.message)
