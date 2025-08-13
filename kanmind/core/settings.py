0b859a8f4621022055118f00a60371a2a180a2f8
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-08-13 17:01:13 +0200

    CORS enabled and tested with frontend

--- kanmind/core/settings.py
+++ kanmind/core/settings.py
@@ -44,6 +44,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 ROOT_URLCONF = 'core.urls'
